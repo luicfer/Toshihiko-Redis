77a413ec8f38e93afe04030204c13db8cba6ce28
luicfer <lucifer4he@gmail.com>
2015-04-11 21:35:26 +0800

    finish test case

--- test/redis.js
+++ test/redis.js
@@ -7,6 +7,12 @@ describe('Array', function(){
     before(function () {
         redis = Redis.create('127.0.0.1:6379',{prefix:'_'});
     });
+    beforeEach(function (done) {
+        redis.setData('test','book',2,{name:'2'}, function (err,data) {
+            data.should.eql('OK');
+            done();
+        })
+    });
     describe('#_getKey', function(){
         it('should return key name', function(){
             redis._getKey('test', 'book', 1).should.eql('_test_book:1');
@@ -14,8 +20,44 @@ describe('Array', function(){
     });
     describe('#setData', function () {
         it('should set data with return ok', function (done) {
-            redis.setData('test','book',1,{name:'fuck'}, function (err,data) {
+            redis.setData('test','book',1,{name:'1'}, function (err,data) {
                 data.should.eql('OK');
+                done();
+            })
+        });
+    });
+    describe('#getData', function () {
+        it('should return one data', function (done) {
+            redis.getData('test','book',1, function (err,data) {
+                data.should.be.an.Array;
+                data.should.eql([{name:'1'}]);
+                done();
+            })
+        });
+        it('should return an array data', function (done) {
+            redis.getData('test','book',[1,2], function (err,data) {
+                data.should.be.an.Array;
+                data.should.have.length(2);
+                data.should.eql([{name:'1'},{name:'2'}]);
+                done();
+            })
+        });
+    });
+
+    describe('#deleteData', function () {
+        it('should delete Data with return 1', function (done) {
+            redis.deleteData('test','book',2, function (err,data) {
+                data.should.eql(1);
+                done();
+            })
+        });
+    });
+
+    describe('#deleteKeys', function () {
+        it('should delete Data with return array res', function (done) {
+            redis.deleteKeys('test','book',[1,2], function (err,data) {
+                data.should.be.an.Array;
+                data.should.eql([1,1]);
                 done();
             })
         });
